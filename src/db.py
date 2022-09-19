a6df4cae0133e4dc2d4919b530021e66e7b744fe
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-09-19 19:54:20 +0300

    Create db.py

---
+++ src/db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+_CONNECTION = "sqlite:///contacts.db"
+
+engine = create_engine(_CONNECTION, echo=True)
+metadata = Base.metadata
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
